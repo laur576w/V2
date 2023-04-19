1267385872faba72e36b55b1293c4f898afaef32
laur576w <114057522+laur576w@users.noreply.github.com>
2023-04-19 09:45:17 +0200

    Lightbox done as how hanne wants

---
+++ js/Lightbox/js/gallery.js
@@ -0,0 +1,20 @@
+const gallery = document.getElementById("gallery")
+gallery.addEventListener('click', overlay)
+
+function overlay(e) {
+    if (e.target.nodeName == "IMG" ) {
+        const div = document.createElement("div")
+        div.setAttribute("class", "overlay")
+        const img = document.createElement("img")
+        img.src = "img/" + e.target.src.split('_')[1]
+        div.appendChild(img)
+        div.addEventListener('click', function(e) {
+            div.remove()
+        })
+        document.body.appendChild(div)
+    }
+    
+}
+
+
+
