f0736b3abc02985cf0d1dd80b9c639502b5ac3e3
laur576w <114057522+laur576w@users.noreply.github.com>
2023-04-20 14:43:17 +0200

    fixed air on function activateTab

--- js/tabbed menu/js/tabbed menu.js
+++ js/tabbed menu/js/tabbed menu.js
@@ -4,21 +4,21 @@ tabs.forEach(tab => {
   tab.addEventListener("click", () => {
     // Set active tab as "active" class and show corresponding content
     activateTab(tab.id);
-    
+
     // Set active tab ID in cookie
     setCookie("activeTabId", tab.id, 3);
   });
 });
 
 function activateTab(tabId) {
-    // Remove "active" class from all tabs and "show" class from all content
-    tabs.forEach(tab => tab.classList.remove("active"));
-    const contents = document.querySelectorAll(".content > div");
-    contents.forEach(content => content.classList.remove("show"));
-    
-    // Add "active" class to selected tab and "show" class to corresponding content
-    const selectedTab = document.getElementById(tabId);
-    selectedTab.classList.add("active");
-    const selectedContent = document.getElementById(`con${tabId.substr(3)}`);
-    selectedContent.classList.add("show");
-  }
+  // Remove "active" class from all tabs and "show" class from all content
+  tabs.forEach(tab => tab.classList.remove("active"));
+  const contents = document.querySelectorAll(".content > div");
+  contents.forEach(content => content.classList.remove("show"));
+
+  // Add "active" class to selected tab and "show" class to corresponding content
+  const selectedTab = document.getElementById(tabId);
+  selectedTab.classList.add("active");
+  const selectedContent = document.getElementById(`con${tabId.substr(3)}`);
+  selectedContent.classList.add("show");
+}
